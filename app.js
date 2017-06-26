be0f0208ab0ee9aa3e7fd52b3115ff5fd43abc0c
Trevor Hall <trhall@trhall-mac.tsi.lan>
2017-06-26 11:16:27 -0700

    wired in front-end buttons with pingback to internal api; api not wired to anything

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const parser = require('body-parser');
 const routing = require('./lib/routing');
 
 app.use(morgan('short'));
+app.use(parser.raw());
+app.use(express.static('static'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.pretty = true;
-
-app.use(express.static('static'))
 
 routing.bootstrap(app);
 
---
+++ static/js/index.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+
+  const submitFeedback = function submitFeedback(isPositive) {
+    const feedbackKey = $.url().param('key');
+    if (!feedbackKey) throw 'Feedback key required';
+
+    $.ajax({
+      url: '/api/submit?r=' + Math.random(),
+      data: {
+        pos: isPositive,
+        key: feedbackKey
+      },
+    }).done(function() {
+    });
+  };
+
+  $('.btn.btn-feedback').click(function() {
+    submitFeedback($(this).hasClass('btn-success'));
+  });
+});
