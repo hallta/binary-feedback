be09f581ae049e0980bfec71834fbea71c1a2d0c
Trevor Hall <trhall@trhall-mac.tsi.lan>
2017-06-26 10:31:53 -0700

    working webui

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const routing = require('./lib/routing');
 
 app.use(morgan('short'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.locals.pretty = true;
 
 app.use(express.static('static'))
 
-app.get('/', function(req, res) {
-    res.render('index.jade', {
-    });
-});
+routing.bootstrap(app);
 
 app.listen(3000, function() {
-    console.log('App running ... ');
+    console.log('binary-feedback running ... ');
 });
