aa7b6d8e17a66a03363f3f1c1f125874298e04ec
Trevor Hall <trhall@trhall-mac.tsi.lan>
2017-06-27 09:44:31 -0700

    better ui for feedback buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function() {
 
-  const submitFeedback = function submitFeedback(isPositive) {
+  const submitFeedback = function submitFeedback(token) {
     const feedbackKey = $.url().param('key');
     if (!feedbackKey) throw 'Feedback key required';
 
     $.ajax({
       url: '/api/save?r=' + Math.random(),
       data: {
-        pos: isPositive,
+        pos: token,
         key: feedbackKey
       },
     }).done(function() {
@@ -16,6 +16,26 @@ $(document).ready(function() {
   };
 
   $('.btn.btn-feedback').click(function() {
-    submitFeedback($(this).hasClass('btn-success'));
+    submitFeedback($(this).attr('feedback-token'));
   });
+
+  // Dynamic height adjustment
+  var adjustButtons = function adjustButtons() {
+      const height = $(window).height() * .8;
+
+      $('a').css('height', height);
+      $('a span').css('padding-top', height * .4);
+
+      $('.fa-4').css('font-size', (height * .02) + 'em')
+  };
+  adjustButtons();
+  $(window).resize(adjustButtons);
+
+  // Add the query content.
+  // Must be in the form ?h1=<something>;p=<lead content>
+  const h1 = $.url().param('h1') || '<Title Missing>';
+  const p = $.url().param('p') || '<Lead content missing>';
+
+  $('div.row.title-row h1').text(h1);
+  $('div.row.description-row p').text(p);
 });
